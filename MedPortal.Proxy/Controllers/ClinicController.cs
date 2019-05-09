9cbf3ecced333db564a3926f9fce2b7c13dc2e01
Mikhail.udot <Mikhail.udot@venafi.com>
2019-05-09 13:57:01 +0300

    1) Add missing files to program; 2) Add middleware to handle exceptions 3) Add logging 4) Fix RestClient

--- MedPortal.Proxy/Controllers/ClinicController.cs
+++ MedPortal.Proxy/Controllers/ClinicController.cs
@@ -1,6 +1,9 @@
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using MedPortal.Proxy.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using RestSharp;
 using RestSharp.Extensions;
 
@@ -10,20 +13,30 @@ namespace MedPortal.Proxy.Controllers
     {
         private readonly IRestClient _restClient;
 
+        private readonly ILogger<ClinicController> _logger;
+
         private readonly string _baseAddress = "https://api.docdoc.ru/public/rest/1.0.12/";
 
-        public ClinicController(IRestClient restClient)
+        public ClinicController(IRestClient restClient, ILogger<ClinicController> logger)
         {
             _restClient = restClient;
+            _logger = logger;
         }
-
+        
+        [HttpGet("api/clinics")]
         public async Task<IActionResult> GetClinics()
         {
             IRestRequest request = new RestRequest(_baseAddress + "clinic/list", Method.GET);
             
-            var result =  _restClient.ExecuteAsync<ClinicListResult>(request, (response) => { });
-
-            return result;
+            var result = await _restClient.ExecuteGetTaskAsync<ClinicListResult>(request);
+            
+            _logger.LogInformation($"Requested to: {request.Resource}, Method: {request.Method}, Response code: {result.StatusCode}");
+            
+            if (result.StatusCode != HttpStatusCode.OK)
+            {
+                throw new HttpRequestException($"Request returned status code: {result.StatusCode}");
+            }
+            return Ok(result.Data);
         }
     }
     
---
+++ MedPortal.Proxy/Startup.cs
@@ -0,0 +1,48 @@
+﻿using MedPortal.Proxy.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RestSharp;
+
+namespace MedPortal.Proxy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddTransient<IRestClient, RestClient>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
