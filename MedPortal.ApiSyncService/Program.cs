019b85653b80bb115109e31a772f385f0764b3ad
Misha3739 <mikhail.udot@yandex.ru>
2019-05-27 16:46:56 +0300

    Fix DI and configuration

--- MedPortal.ApiSyncService/Program.cs
+++ MedPortal.ApiSyncService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using AutoMapper;
 using MedPortal.Data.DTO;
 using MedPortal.Data.Persistence;
@@ -11,14 +12,38 @@ using RestSharp;
 using RestSharp.Authenticators;
 using ServiceLocator;
 using System.Configuration;
+using System.Globalization;
+using System.Threading.Tasks;
 using MedPortal.ApiSyncService.Engine;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Logging;
 
 namespace MedPortal.ApiSyncService
 {
 	class Program
 	{
 		
-		static void Main(string[] args)
+		static void Main(string[] args) {
+			RegisterServices();
+			
+
+			var engine = DIProvider.ServiceProvider.GetService<SyncEngine>();
+
+			try {
+				engine.SyncAll().GetAwaiter().GetResult();
+			} catch (Exception e) {
+				Console.WriteLine(e);
+			}
+			
+
+			DisposeServices();
+
+			Console.ReadLine();
+
+		}
+
+		private static void RegisterServices()
 		{
 			IServiceCollection services = new ServiceCollection();
 			var mappingConfig = new MapperConfiguration(mc =>
@@ -28,38 +53,56 @@ namespace MedPortal.ApiSyncService
 
 			services.AddTransient<IMapper>(options => mappingConfig.CreateMapper());
 
-			DIProvider.ServiceProvider = services.BuildServiceProvider();
 
-			var apiConfiguration = (ApiConfiguration)ConfigurationManager.GetSection(nameof(ApiConfiguration));
 
-			services.AddScoped<IRestClient>(service => new RestClient(apiConfiguration.BaseAddress)
+			var apiConfiguration = new ApiConfiguration() {
+				BaseAddress = ConfigurationManager.AppSettings["BaseAddress"],
+				Login = ConfigurationManager.AppSettings["Login"],
+				Password = ConfigurationManager.AppSettings["Password"]
+			};
+
+
+			services.AddTransient<IRestClient>(service => new RestClient(apiConfiguration.BaseAddress)
 			{
 				Authenticator = new HttpBasicAuthenticator(apiConfiguration.Login, apiConfiguration.Password)
 			});
 
 			services.AddDbContext<IDataContext, DataContext>(option => option.UseSqlServer(
 				ConfigurationManager.ConnectionStrings["MedPortal"].ConnectionString
-			));
+			), ServiceLifetime.Transient);
 
 			services.AddScoped<IUnitOfWork, UnitOfWork>();
 			services.AddTransient<IRepository<Log>, LogRepository>();
 			services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
 			services.AddTransient(typeof(IHighloadedRepository<>), typeof(HighloadedRepository<>));
 
-			var engine = new SyncEngine(
-				DIProvider.ServiceProvider.GetService<IHighloadedRepository<HCity>>(),
-				DIProvider.ServiceProvider.GetService<IHighloadedRepository<HDistrict>>(),
-				DIProvider.ServiceProvider.GetService<IHighloadedRepository<HStation>>(),
-				DIProvider.ServiceProvider.GetService<IHighloadedRepository<HSpeciality>>(),
-				DIProvider.ServiceProvider.GetService<IRepository<HClinicStations>>(),
-				DIProvider.ServiceProvider.GetService<IHighloadedRepository<HClinic>>(),
-				DIProvider.ServiceProvider.GetService<IHighloadedRepository<HStreet>>()
-				);
+			services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
 
-			engine.SyncAll().GetAwaiter().GetResult();
+			services.AddTransient<SyncEngine>();
 
-			Console.ReadLine();
+			services.AddLogging(configure => configure.AddConsole());
 
+			services.Configure<RequestLocalizationOptions>(options =>
+				new RequestLocalizationOptions {
+					DefaultRequestCulture = new RequestCulture(new CultureInfo("en-US")),
+					SupportedCultures = new List<CultureInfo> {
+						new CultureInfo("ru-RU")
+					}
+				});
+
+			DIProvider.ServiceProvider = services.BuildServiceProvider();
+		}
+
+		private static void DisposeServices()
+		{
+			if (DIProvider.ServiceProvider == null)
+			{
+				return;
+			}
+			if (DIProvider.ServiceProvider is IDisposable)
+			{
+				((IDisposable)DIProvider.ServiceProvider).Dispose();
+			}
 		}
 	}
 
