9af06db9fc2b6e1f732ac978f0ffe31686a2dea4
Misha3739 <mikhail.udot@yandex.ru>
2019-08-01 15:34:37 +0300

    Work on clinic-cards appearance

--- MedPortal.WebApiClient/ClientApp/src/app/data/clinic.ts
+++ MedPortal.WebApiClient/ClientApp/src/app/data/clinic.ts
@@ -4,6 +4,8 @@ export interface IClinic {
       id: number,
       alias: string,
       name: string,
+      logo: string;
+      address: string,
       latitude: number,
       longtitude: number,
       doctors: IDoctor[];
--- MedPortal.WebApiClient/ClientApp/src/app/services/clincs-service.ts
+++ MedPortal.WebApiClient/ClientApp/src/app/services/clincs-service.ts
@@ -9,7 +9,9 @@ export class ClinicsService {
     clinics.push({
       id: 1,
       name: 'Центр Диетологии',
+      logo: 'https://cdn4.docdoc.pro/clinic/logo/min_14179.png?1562319645',
       alias: 'Center_Dietology',
+      address:'Малая Морская 22',
       latitude: 76,
       longtitude: 66,
       doctors: []
@@ -18,6 +20,8 @@ export class ClinicsService {
       id: 2,
       alias: 'Center_Prostatology',
       name: 'Центр Простатологии',
+      logo: 'https://cdn4.docdoc.pro/clinic/logo/logo_default.gif?1557801558',
+      address: 'Большой пр ПС 33',
       latitude: 76,
       longtitude: 66,
       doctors: []
@@ -26,6 +30,8 @@ export class ClinicsService {
       id: 3,
       alias: 'Center_FamilyMedicine',
       name: 'Центр Семейной медицины',
+      logo: 'https://cdn4.docdoc.pro/clinic/logo/logo_default.gif?1562657624',
+      address: 'Московский пр 143',
       latitude: 76,
       longtitude: 66,
       doctors: []
