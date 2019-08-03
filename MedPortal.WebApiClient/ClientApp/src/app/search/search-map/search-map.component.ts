fda611c91236015169cfb0d05c7eeafeba466d3f
Misha3739 <mikhail.udot@yandex.ru>
2019-08-03 12:26:28 +0300

    Add navigation to doctors and clinics

--- MedPortal.WebApiClient/ClientApp/src/app/search/search-map/search-map.component.ts
+++ MedPortal.WebApiClient/ClientApp/src/app/search/search-map/search-map.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, Directive, ViewEncapsulation } from '@ang
 import { } from 'googlemaps';
 import { SearchInfoService } from '../../services/search-info-service';
 import { Subscription } from 'rxjs';
-import { ActivatedRoute, Router, Params } from '@angular/router';
+import { ActivatedRoute, Router, Params, NavigationEnd } from '@angular/router';
 import { ISearchCategory } from '../../data/search-info';
 import { SearchInfoType } from '../../data/search-info-type';
 import { Select2OptionData } from 'ng-select2';
@@ -22,10 +22,15 @@ export class SearchMapComponent implements OnInit {
   displayCategories: Array<Select2OptionData>;
   routeParamsSubscription: Subscription;
   city: string;
-  isSelected = false;
+  singleItemSelected = false;
 
   navigateToResource: SearchInfoType;
   navigateToAlias: string;
+
+  clinicsSearchSelected = false;
+  doctorsSearchSelected = false;
+
+  currentUrl: string;
 
   constructor(private searchInfoService: SearchInfoService, private clinicsService: ClinicsService, private route: ActivatedRoute, private router: Router) { }
 
@@ -34,6 +39,7 @@ export class SearchMapComponent implements OnInit {
 
     this.routeParamsSubscription = this.route.params.subscribe(
       (params: Params) => {
+        this.currentUrl = this.router.url;
         this.city = params['city'];
         this.categories = this.searchInfoService.getSearchInfo(this.city);
         this.displayCategories = this.toDisplayData();
@@ -80,7 +86,7 @@ export class SearchMapComponent implements OnInit {
       let splitted = value.split('@', 2);
       this.navigateToResource = parseInt(splitted[0]);
       this.navigateToAlias = splitted[1];
-      this.isSelected = true;
+      this.singleItemSelected = this.navigateToResource === SearchInfoType.clinic || this.navigateToResource === SearchInfoType.doctor;
       console.log('Will navigate to ' + SearchInfoType[this.navigateToResource] + ' alias: ' + this.navigateToAlias);
 
       if (this.navigateToResource === SearchInfoType.clinic) {
@@ -95,18 +101,24 @@ export class SearchMapComponent implements OnInit {
   }
 
   onRedirect() {
-    switch (this.navigateToResource) {
-      case SearchInfoType.clinic:
-        this.router.navigate(['clinic/' + this.navigateToAlias]);
-        break;
-      case SearchInfoType.doctor:
-        this.router.navigate(['doctor/' + this.navigateToAlias]);
-        break;
-      case SearchInfoType.speciality:
-        this.router.navigate(['speciality/' + this.navigateToAlias]);
-        break;
+    if (this.singleItemSelected) {
+      switch (this.navigateToResource) {
+        case SearchInfoType.clinic:
+          this.router.navigate(['clinic/' + this.navigateToAlias]);
+          break;
+        case SearchInfoType.doctor:
+          this.router.navigate(['doctor/' + this.navigateToAlias]);
+          break;
+      }
+    } else {
+      if (this.clinicsSearchSelected && this.doctorsSearchSelected) {
+
+      } else if (this.clinicsSearchSelected && !this.doctorsSearchSelected) {
+        this.router.navigate([this.currentUrl + '/clinics']);
+      } else if (!this.clinicsSearchSelected && this.doctorsSearchSelected) {
+        this.router.navigate([this.currentUrl + '/doctors']);
+      }
     }
-   
   }
 
   displayCategory(type: SearchInfoType) : string {
