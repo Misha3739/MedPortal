79e1255531829faae7b61c721f3c653347dfd5a6
Misha3739 <mikhail.udot@yandex.ru>
2019-08-09 19:58:56 +0300

    Apply maps to show clinic's location

--- MedPortal.WebApiClient/ClientApp/src/app/clinics/clinic-card/clinic-card.component.ts
+++ MedPortal.WebApiClient/ClientApp/src/app/clinics/clinic-card/clinic-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { IClinic } from '../../data/clinic';
 
 @Component({
@@ -8,10 +8,41 @@ import { IClinic } from '../../data/clinic';
 })
 export class ClinicCardComponent implements OnInit {
   @Input() clinic: IClinic;
+  @ViewChild('gmap', { static: true }) gmapElement: any;
+  map: google.maps.Map;
+
+  isMapShown: boolean = false;
+  isMapInitialized: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
 
+  initMaps(latitude: number, longitude: number) {
+    let mapProp = {
+      center: new google.maps.LatLng(latitude, longitude),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+  }
+
+  showMap() {
+    this.isMapShown = !this.isMapShown;
+    if (this.isMapShown && !this.isMapInitialized) {
+      let latitude = this.clinic.latitude;
+      if (latitude == 0) {
+        latitude = this.clinic.city.latitude;
+      }
+      let longitude = this.clinic.latitude;
+      if (longitude == 0) {
+        longitude = this.clinic.city.longitude;
+      }
+      this.initMaps(latitude, longitude);
+      this.isMapInitialized = true;
+    }
+  }
 }
