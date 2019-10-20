e45deaa2bd9f989832f03e5b2fb1b265eb5d2f64
Misha3739 <mikhail.udot@yandex.ru>
2019-10-20 23:37:45 +0300

    Issue 7 (fix)

--- MedPortal.WebApiClient/ClientApp/src/app/clinics/clinic-card/clinic-card.component.ts
+++ MedPortal.WebApiClient/ClientApp/src/app/clinics/clinic-card/clinic-card.component.ts
@@ -8,41 +8,10 @@ import { IClinic } from '../../data/clinic';
 })
 export class ClinicCardComponent implements OnInit {
   @Input() clinic: IClinic;
-  @ViewChild('gmap', { static: true }) gmapElement: any;
-  map: google.maps.Map;
-
-  isMapShown: boolean = false;
-  isMapInitialized: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
     
-  }
-
-  initMaps(latitude: number, longitude: number) {
-    let mapProp = {
-      center: new google.maps.LatLng(latitude, longitude),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.HYBRID
-    };
-
-    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-  }
-
-  showMap() {
-    this.isMapShown = !this.isMapShown;
-    if (this.isMapShown && !this.isMapInitialized) {
-      let latitude = this.clinic.latitude;
-      if (latitude == 0) {
-        latitude = this.clinic.city.latitude;
-      }
-      let longitude = this.clinic.latitude;
-      if (longitude == 0) {
-        longitude = this.clinic.city.longitude;
-      }
-      this.initMaps(latitude, longitude);
-      this.isMapInitialized = true;
-    }
   }
 }
